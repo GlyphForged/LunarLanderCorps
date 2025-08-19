40190c79e654aef9a1545520552141f06c0585fa
j <no@notrightheckingnow.com>
2025-08-19 13:59:18 -0500

    Revert "first-pass at damage calculation and display"
    This reverts commit a0576486e8aa08c24e2db5ee9eaf2ff3c7151f98.

--- scripts/ui/hud_lander_pos.gd
+++ scripts/ui/hud_lander_pos.gd
@@ -10,13 +10,11 @@ func _process(_delta):
 
 		var h_vel := Vector2(vel.x, vel.z).length()
 		var v_vel: float = abs(vel.y)
-		var dmg: float = lander_bod.current_damage
 
-		text = "POS: %4.2f, %4.2f\nH_SPD: %4.2f\nV_SPD: %4.2f\nDMG: %4.2f\nFPS: %2d" % [
+		text = "POS: %4.2f, %4.2f\nH_SPD: %4.2f\nV_SPD: %4.2f\nFPS: %2d" % [
 			pos.x,
 			pos.z,
 			h_vel,
 			v_vel,
-			dmg,
 			Engine.get_frames_per_second()
 		]
