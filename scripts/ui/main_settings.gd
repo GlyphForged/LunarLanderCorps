325a6589dcc8fc46851a6dc224525aeaad85fe6b
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-08-23 10:44:53 -0400

    Added Maaacks Input Remapping - Gave up on re-inventing the wheel - Pause Menu fully functional - Setting menu is now its own scene - Settings menu is instantiated as needed and freed on exit - Settings menu utilizes signal bus for closure - Pause menu and main menu connect to signal bus for settings closure TODO: - Fix input menu margin weirdness - Friendly names for inputs

--- scripts/ui/main_settings.gd
+++ scripts/ui/main_settings.gd
@@ -7,29 +7,15 @@ const WINDOW_RESOLUTION_OPTIONS: Array[String] = [
 	"1280 x 720",
 ]
 
-@onready var actions := InputMap.get_actions()
+func _ready() -> void:
+	%BackBtn.call_deferred("grab_focus")
 
-#func _ready() -> void:
-#var input_key: InputEventKey
-#var input_action: InputEventAction
-	#for action in actions:
-		#if !action.begins_with("ui_"):
-			#var action_button = InputMap.action_get_events(action)
-			#for bttn in action_button:
-				#if bttn.get_class() = InputEventKey:
-					#input_key = bttn
-				#elif bttn.get_class() = InputEventAction:
-					#input_action = bttn
-
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_VISIBILITY_CHANGED:
-		if is_visible():
-			%BackBtn.grab_focus()
+	var margin_container: MarginContainer = self.get_node("MarginContainer")
+	Util.set_margins(margin_container, 0.2, 0.15)
 
 func _on_back_btn_pressed() -> void:
-	var main_menu = get_tree().get_node("MainMenu")
-	main_menu.show()
-	# TODO: Show the rest
+	Signals.settings_closed.emit()
+	self.queue_free()
 
 func _on_resolution_selected(index: int) -> void:
 	match index:
@@ -50,7 +36,6 @@ func _on_fs_toggled(toggled_on: bool) -> void:
 
 func _on_vol_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
-	print(str(AudioServer.get_bus_volume_db(0)))
 
 func _on_mute_toggled(toggled_on: bool) -> void:
 		AudioServer.set_bus_mute(0, toggled_on)
--- scripts/utils/util.gd
+++ scripts/utils/util.gd
@@ -9,3 +9,11 @@ enum GAME_SCENES {GAME, MENU}
 enum CAMERA_MODE {HORIZON_LOCK, FREE}
 
 # === UTIL FUNCTIONS ===
+func set_margins(node: MarginContainer, hPerc: float, vPerc: float) -> void:
+	var margin_container: MarginContainer = node
+	var hmargin: int = floor(hPerc * get_window().get_visible_rect().size.x)
+	var vmargin: int = floor(vPerc * get_window().get_visible_rect().size.y)
+	margin_container.add_theme_constant_override("margin_left", hmargin)
+	margin_container.add_theme_constant_override("margin_right", hmargin)
+	margin_container.add_theme_constant_override("margin_top", vmargin)
+	margin_container.add_theme_constant_override("margin_bottom", vmargin)
