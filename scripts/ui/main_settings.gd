fb38042749e9c235566e32c7c840c72f70aee004
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-08-26 17:30:17 -0400

    Updated Fonts to be dynamic on resize.

--- scripts/ui/main_settings.gd
+++ scripts/ui/main_settings.gd
@@ -11,16 +11,17 @@ func _ready() -> void:
 	%BackBtn.call_deferred("grab_focus")
 
 	var margin_container: MarginContainer = self.get_node("MarginContainer")
-	Util.set_margins(margin_container, 0.2, 0.15)
+	Util.set_margins(margin_container, 0.15, 0.1)
 
 func _on_back_btn_pressed() -> void:
 	Signals.settings_closed.emit()
 	self.queue_free()
 
 func _on_resolution_selected(index: int) -> void:
-	DisplayServer.window_set_size(
-		Util.RESOLUTIONS.get(Util.RESOLUTIONS_INDEX[index]
-	))
+	var res: Vector3i = Util.RESOLUTIONS.get(Util.RESOLUTIONS_INDEX[index])
+	DisplayServer.window_set_size(Vector2i(res.x, res.y))
+	Util.settings.font_size = res.z
+	Util.project_theme.default_font_size = res.z
 	Util.settings.resolution_index = index
 	Util.save_settings()
 
@@ -44,15 +45,12 @@ func _on_mute_toggled(toggled_on: bool) -> void:
 		Util.save_settings()
 
 func _on_d_font_toggled(toggled_on: bool) -> void:
-	var project_theme: Theme = load(Util.THEME_ID)
-	var def_font: FontFile = load(Util.DEF_FONT_ID)
-	var dys_font: FontFile = load(Util.DYS_FONT_ID)
 	if toggled_on:
-		project_theme.default_font = dys_font
-		project_theme.default_font_size = 12
+		Util.project_theme.default_font = Util.dys_font
+		Util.project_theme.default_font_size = 12
 		Util.settings.dyslexic_font = true
 	else:
-		project_theme.default_font = def_font
-		project_theme.default_font_size = Util.settings.font_size
+		Util.project_theme.default_font = Util.def_font
+		Util.project_theme.default_font_size = Util.settings.font_size
 		Util.settings.dyslexic_font = false
 	Util.save_settings()
